8a0ca44cddcba1c8c7d90a177612f8078707039c
Galbadrakh Battur <galbadrakhbattur@Galbadrakhs-MBP.attlocal.net>
2024-05-04 14:10:53 -0500

    calls endpoints and connects to db

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,57 @@
 package main
 
 import (
-	"log"
+	"database/sql"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"snippetbox.galaa.net/internal/models"
 )
 
+type application struct {
+	logger   *slog.Logger
+	snippets *models.SnippetModel
+}
+
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	// Add flags
+	addr := flag.String("addr", ":4000", "HTTP Network address")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL Datasource name")
+	flag.Parse()
 
-	log.Print("starting server on: 4000")
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
 
+	app := &application{
+		logger:   logger,
+		snippets: &models.SnippetModel{DB: db},
+	}
+
+	logger.Info("Starting server on", "addr", *addr)
+
+	err = http.ListenAndServe(*addr, app.routes())
+	logger.Error(err.Error())
+	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+	return db, nil
 }
